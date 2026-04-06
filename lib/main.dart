4b4dc2dae52210741236182bd73db8521608766c
Joo Seu Yoon <ajanajan1217@gmail.com>
2026-04-06 22:17:48 +0900

    supabase connect init

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,17 @@
 import 'package:flutter/material.dart';
 import 'package:tracking_diary/l10n/app_localizations.dart';
 import 'package:tracking_diary/pages/login.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await dotenv.load(fileName: '.env');
+
+  await Supabase.initialize(
+    url: dotenv.env['SUPABASE_URL']!,
+    anonKey: dotenv.env['SUPABASE_ANON_KEY']!,
+  );
   runApp(const MyApp());
 }
 
